bd1f5f5eff6896e1b93b34bf48d3f0eb7e8ce746
Fimber Eze <fimber.eze@bu-uk.co.uk>
2024-04-24 17:09:08 +0100

    Setup Vue app

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Fimber',
+            age: 33
+        }
+    }
+})
+
+app.mount('#app')
