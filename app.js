5829b72a01a4aec62341743c437dd300675eb232
Fimber Eze <fimber.eze@bu-uk.co.uk>
2024-05-10 17:48:51 +0100

    Created a app widget

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const app = Vue.createApp({
     data() {
         return {
-            title: 'The Final Empire',
-            author: 'Fimber',
-            age: 33
+            url: 'http://www.thenetninja.co.uk',
+            showBooks: true,
+            books: [
+                {title: 'The Final Empire', author: 'Fimber', age: 33, isFav: true},
+                {title: 'The of the Kings', author: 'Brandon Sanders', age: 33, isFav: false},
+                {title: 'The Final Empire', author: 'Fimber', age: 33, isFav: true}
+            ]
+        }
+    },
+    methods: {
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
+        },
+
+        toggleFav(book){
+            book.isFav = !book.isFav
+        }
+    },
+    computed: {
+        filteredBooks(){
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
